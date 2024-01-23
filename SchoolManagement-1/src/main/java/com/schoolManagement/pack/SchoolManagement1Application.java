04d1c57a78a6e10dba47d5e2c5d34db8d734e652
shubham Kumar Nimje <shubbham13@gmail.com>
2024-01-23 15:54:29 +0530

    intial

---
+++ SchoolManagement-1/src/main/java/com/schoolManagement/pack/SchoolManagement1Application.java
@@ -0,0 +1,13 @@
+package com.schoolManagement.pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagement1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagement1Application.class, args);
+	}
+
+}
