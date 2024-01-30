241e15ae4f4effc66ecba718ee0b7c1bf893896f
shubham Kumar Nimje <shubbham13@gmail.com>
2024-01-30 18:32:13 +0530

    intial

--- SchoolManagement-1/src/main/java/com/schoolManagement/pack/Controller/UserPage.java
+++ SchoolManagement-1/src/main/java/com/schoolManagement/pack/Controller/UserPage.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.client.RestTemplate;
 
 import com.schoolManagement.pack.Repositories.MarksheetRepo;
 import com.schoolManagement.pack.Repositories.StudentRepo;
@@ -15,10 +17,24 @@ import jakarta.servlet.http.HttpServletResponse;
 @Controller
 public class UserPage {
 	
-	@ResponseBody
-	@RequestMapping("/register-user/{suser}/{spassword}/{semail}")
-	public String register (HttpServletRequest req , HttpServletResponse res , @PathVariable("suser") String suser,@PathVariable("spassword")  String spassword,@PathVariable("semail") String semail) {
-		
+	
+	@RequestMapping("/SchoolManagement-1/{userID}/{password}/{selector}")
+	public ModelAndView register (HttpServletRequest req , HttpServletResponse res , @PathVariable("userID") String userID,@PathVariable("password")  String password,@PathVariable("selector") String selector) {
+		ModelAndView mv = new ModelAndView();
+		try {
+			
+			if(selector.contentEquals("student")) {
+				mv.setViewName("studentOperation");
+			}
+			else if (selector.contentEquals("teacher")) {
+				mv.setViewName("teacherOperations");
+			}
+			else
+				mv.setViewName("failed");
+			
+		}catch (Exception e) {
+			mv.setViewName("failed");
+		}
 	/*Student s = new Student();
 	s.setSuser(suser);
 	s.setSpassword(spassword);
@@ -39,10 +55,12 @@ public class UserPage {
 		return "Register Failed";
 	}*/
 		
-		return "yetToComplete";
 		
+		return mv;
 		
 	}
+	
+	
 
 	
 	
